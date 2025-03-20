fca08de0567d6b5b050e24eacc375dc6fc9ecf49
luan005 <bezerra.luan@academico.ifpb.edu.br>
2025-03-20 08:23:00 -0300

    Delete ATT_REACT_IFPB/Pweb.IFPB directory

--- ATT_REACT_IFPB/Pweb.IFPB/hello-react-app/src/views/Home.jsx
+++
@@ -1,7 +0,0 @@
-import Main from '../components/Main';
-
-const Home = () => {
-  return <Main></Main>;
-};
-
-export default Home;
--- ATT_REACT_IFPB/Pweb.IFPB/hello-react-app/src/views/Sobre.jsx
+++
@@ -1,9 +0,0 @@
-const Sobre = () => {
-  return (
-    <>
-      <div>Sobre</div>
-    </>
-  );
-};
-
-export default Sobre;
